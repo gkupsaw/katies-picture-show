7cb25d01869bb294cbca2bbe657cc4e293154893
EnriqueVega1995 <vegacampos1995@gmail.com>
2019-10-30 13:59:15 -0600

    Added Animator and script transitions controller

---
+++ Assets/Character/AnimatorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class AnimatorController : MonoBehaviour {
+    
+     public Animator animator;
+    
+    // Use this for initialization
+    void Start () {
+       animator = GetComponent<Animator>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        bool walk = Input.GetButtonDown("Fire1");
+        
+
+        if (Input.GetKey(KeyCode.E))
+       {
+        walk=true;
+       }
+       animator.SetBool("walk", walk);
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            animator.SetTrigger("Die");
+        }
+    }
+}
