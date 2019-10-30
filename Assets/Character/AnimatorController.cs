b80dea77c7c411a89d241030f729bd42e7a11aee
EnriqueVega1995 <vegacampos1995@gmail.com>
2019-10-30 16:45:51 -0600

    Added animator and animations

--- Assets/Character/AnimatorController.cs
+++ Assets/Character/AnimatorController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 public class AnimatorController : MonoBehaviour {
     
      public Animator animator;
+     public NavMeshAgent nav;
     
     // Use this for initialization
     void Start () {
@@ -15,13 +17,21 @@ public class AnimatorController : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         bool walk = Input.GetButtonDown("Fire1");
-        
-
-        if (Input.GetKey(KeyCode.E))
-       {
-        walk=true;
+        bool turn = Input.GetButtonDown("Fire1");
+        if(nav.angularSpeed>1){
+            walk=false;
+            //turn=true;
+        }
+        if(nav.remainingDistance>1){
+            walk=true;
+            //turn=false;
+            Debug.Log("massss");
+        }
+            animator.SetBool("walk", walk);
+            animator.SetBool("turn", turn);
+        if (Input.GetKey(KeyCode.E)){
+            walk=true;
        }
-       animator.SetBool("walk", walk);
     }
 
     void OnCollisionEnter(Collision col) {
---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+    
+    public class MoveTo : MonoBehaviour {
+       
+       public Transform goal;
+       
+       void Start () {
+           
+       }
+       void Update () {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = goal.position;
+       }
+    }
