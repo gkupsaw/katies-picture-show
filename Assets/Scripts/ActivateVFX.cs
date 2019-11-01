7152ef1a61b5c443c066966651bb6ec8dfd712de
EnriqueVega1995 <vegacampos1995@gmail.com>
2019-11-01 17:12:12 -0600

    Added switch between scenes and

---
+++ Assets/Scripts/ActivateVFX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ActivateVFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string scene;
+    public GameObject VR;
+    void Start()
+    {
+        
+           //VR.transform.position = new Vector3(3.5f,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+       {
+           Valve.VR.OpenVR.System.ResetSeatedZeroPose();
+           SceneManager.LoadSceneAsync("Dynamic Suprematism");
+           VR.transform.position = new Vector3(3.5f,0,0);
+       }
+    }
+     void OnTriggerEnter(Collider n)
+    {
+        if (n.gameObject.tag == "MainCamera")
+        {
+            Initiate.Fade(scene,Color.black, 1.0f);
+            Debug.Log("Inside");
+            //player.transform.parent = transform;
+        }
+    }
+}
